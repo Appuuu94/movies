9f61498d9fd18b889384f2d53301a0cbbd5efef8
Appuuu94 <apurbamura1@gmail.com>
2025-01-04 10:05:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -180,7 +180,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton("Hᴇʟᴘ ⚙️", callback_data='features'),
                             InlineKeyboardButton('Aʙᴏᴜᴛ 💌', callback_data=f'about')
                         ],[
-                            InlineKeyboardButton('Backup Channel',https://t.me/Cinemaghar_Backup'),
+                            InlineKeyboardButton('Backup Channel',url=f"https://t.me/Cinemaghar_Backup"),
                             InlineKeyboardButton('Rᴇғᴇʀ ⚜️', callback_data="reffff")
                         ],[
                             InlineKeyboardButton('Mᴏsᴛ Sᴇᴀʀᴄʜ 🔍', callback_data="mostsearch"),
@@ -209,7 +209,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton("Hᴇʟᴘ ⚙️", callback_data='features'),
                             InlineKeyboardButton('Aʙᴏᴜᴛ 💌', callback_data=f'about')
                         ],[
-                            InlineKeyboardButton('Pʀᴇᴍɪᴜᴍ 🎫', callback_data='seeplans'),
+                            InlineKeyboardButton('InlineKeyboardButton('Backup Channel',url=f"https://t.me/Cinemaghar_Backup'),
                             InlineKeyboardButton('Rᴇғᴇʀ ⚜️', callback_data="reffff")
                         ],[
                             InlineKeyboardButton('Mᴏsᴛ Sᴇᴀʀᴄʜ 🔍', callback_data="mostsearch"),
